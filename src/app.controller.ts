1998fb3d3b521e95a8dca40b63212d76d5a9da27
susukida <susukida@loremipsum.co.jp>
2023-02-24 09:24:33 +0900

    Commit

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,47 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Post, RawBodyRequest, Req, Res } from '@nestjs/common';
+import { createHmac, timingSafeEqual } from 'crypto';
+import { Request, Response } from 'express';
 import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  /**
+   * Chatwork Webhook 用のエンドポイントです。
+   * @param req Express リクエストオブジェクト
+   * @param res Express レスポンスオブジェクト
+   */
+  @Post('api/v1/chatwork/webhook')
+  onApiV1ChatworkWebhook(
+    @Req() req: RawBodyRequest<Request>,
+    @Res() res: Response,
+  ) {
+    // リクエスト署名を検証します。
+    const webhookToken = process.env.WEBHOOK_TOKEN;
+    const privateKey = Buffer.from(webhookToken).toString('base64');
+    const actualSignature = createHmac('sha256', privateKey)
+      .update(req.rawBody)
+      .digest('base64');
+
+    const expectedSignature = req.headers[
+      'x-chatworkwebhooksignature'
+    ] as string;
+
+    const isSignatureVerified = timingSafeEqual(
+      Buffer.from(actualSignature),
+      Buffer.from(expectedSignature),
+    );
+
+    if (!isSignatureVerified) {
+      res.status(400).send('Bad Request: !isSignatureVerified');
+      console.warn(JSON.stringify({ actualSignature, expectedSignature }));
+      return;
+    }
+
+    // リクエストの内容を出力します。
+    console.info(req.rawBody.toString());
+
+    res.status(200).end('OK');
   }
 }
---
+++ test/chatwork-webhook.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { createHmac } from 'crypto';
+import fetch from 'node-fetch';
+
+describe('Chatwork Webhook', () => {
+  it('/api/v1/chatwork/webhook', async () => {
+    const requestBody = JSON.stringify({
+      webhook_setting_id: '12345',
+      webhook_event_type: 'mention_to_me',
+      webhook_event_time: 1498028130,
+      webhook_event: {
+        from_account_id: 123456,
+        to_account_id: 1484814,
+        room_id: 567890123,
+        message_id: '789012345',
+        body: '[To:1484814]おかずはなんですか？',
+        send_time: 1498028125,
+        update_time: 0,
+      },
+    });
+
+    const webhookToken = process.env.WEBHOOK_TOKEN;
+    const privateKey = Buffer.from(webhookToken).toString('base64');
+    const webhookSignature = createHmac('sha256', privateKey)
+      .update(requestBody)
+      .digest('base64');
+
+    const fetchUrl = process.env.BASE_URL + '/api/v1/chatwork/webhook';
+    const fetchResponse = await fetch(fetchUrl, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Chatwork-Webhook/1.0.0',
+        'X-Chatworkwebhooksignature': webhookSignature,
+      },
+      body: requestBody,
+    });
+
+    console.log(fetchResponse.status);
+    console.log(await fetchResponse.text());
+  });
+});
